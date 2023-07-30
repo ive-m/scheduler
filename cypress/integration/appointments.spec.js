641c6e8e162b3641c5cbff919794a71f04666877
ive-m <ivesita.maria@gmail.com>
2023-07-29 20:21:02 -0400

    Add screenshots

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -11,12 +11,12 @@ describe("Appointments", () => {
     //cy.wait(2000);
    });
   it("should book an interview", () => {
-    cy.get("[alt=Add")
+    cy.get("[alt=Add]")
     .first()  
     .click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    cy.get("[alt='Sylvia Palmer']").click();
+    cy.get("[alt='Sylvia Palmer']").click()
 
     cy.contains("Save").click();
 
